b08c1199e3b66f4247bc588f58ae99de751a10d1
Leejungsuk96 <svwsv3462@gmail.com>
2023-12-12 17:11:32 +0900

    [추가] 고객정보 스크립트 추가중

---
+++ Assets/Scripts/Customer/CustomerInformation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CustomerInformation
+{
+    public string CustomerName;
+    public int CustomerAccountBalance;
+    public int CustomerWalletBalance;
+}
