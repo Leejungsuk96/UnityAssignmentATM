86b1e413b91f778f38cd5af94b2f6790aa082881
Leejungsuk96 <svwsv3462@gmail.com>
2023-12-13 13:29:27 +0900

    [추가] 입금 기능 구현

--- Assets/Scripts/UIController/UIController.cs
+++ Assets/Scripts/UIController/UIController.cs
@@ -10,8 +10,10 @@ public class UIController : MonoBehaviour
     public GameObject DepositMode;
     public CustomerInformation customerInfo;
     public GameObject WarningPanel;
-    public Text DirectAmountTxt;
-    private int DirectAmount = 0;
+    public Text DirectWithdrawAmountTxt;
+    private int DirectWithdrawAmount = 0;
+    public Text DirectDepositAmountTxt;
+    private int DirectDepositAmount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -90,12 +92,67 @@ public class UIController : MonoBehaviour
 
     public void ClickDirectWithdraw()
     {
-        
-        DirectAmount = int.Parse(DirectAmountTxt.text);
-        if(customerInfo.CustomerAccountBalance >= DirectAmount)
+
+        DirectWithdrawAmount = int.Parse(DirectWithdrawAmountTxt.text);
+        if(customerInfo.CustomerAccountBalance >= DirectWithdrawAmount)
         {
-            customerInfo.CustomerAccountBalance -= DirectAmount;
-            customerInfo.CustomerWalletBalance += DirectAmount;
+            customerInfo.CustomerAccountBalance -= DirectWithdrawAmount;
+            customerInfo.CustomerWalletBalance += DirectWithdrawAmount;
+        }
+        else
+        {
+            WarningPanel.SetActive(true);
+        }
+    }
+
+    public void ClickTenDeposit()
+    {
+        if (customerInfo.CustomerWalletBalance >= 10000)
+        {
+            customerInfo.CustomerAccountBalance += 10000;
+            customerInfo.CustomerWalletBalance -= 10000;
+        }
+
+        else
+        {
+            WarningPanel.SetActive(true);
+        }
+    }
+    public void ClickThirtyDeposit()
+    {
+        if (customerInfo.CustomerWalletBalance >= 30000)
+        {
+            customerInfo.CustomerAccountBalance += 30000;
+            customerInfo.CustomerWalletBalance -= 30000;
+        }
+
+        else
+        {
+            WarningPanel.SetActive(true);
+        }
+    }
+    public void ClickFiftyDeposit()
+    {
+        if (customerInfo.CustomerWalletBalance >= 50000)
+        {
+            customerInfo.CustomerAccountBalance += 50000;
+            customerInfo.CustomerWalletBalance -= 50000;
+        }
+
+        else
+        {
+            WarningPanel.SetActive(true);
+        }
+    }
+
+    public void ClickDirectDeposit()
+    {
+
+        DirectDepositAmount = int.Parse(DirectDepositAmountTxt.text);
+        if (customerInfo.CustomerWalletBalance >= DirectDepositAmount)
+        {
+            customerInfo.CustomerAccountBalance += DirectDepositAmount;
+            customerInfo.CustomerWalletBalance -= DirectDepositAmount;
         }
         else
         {
